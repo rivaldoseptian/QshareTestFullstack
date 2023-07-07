4c29609a91da7d7d4f96023af3ded8cc438d07c1
rivaldoseptian <rivaldo.septian121@gmail.com>
2023-07-07 20:50:13 +0700

    skeleton frontend

---
+++ frontend/src/pages/home.vue
@@ -0,0 +1,22 @@
+<script></script>
+<template>
+  <section class="container mx-auto p-6 font-mono">
+    <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+      <div class="w-full overflow-x-auto">
+        <table class="w-full">
+          <thead>
+            <tr
+              class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600"
+            >
+              <th class="px-4 py-3">No</th>
+              <th class="px-4 py-3">Title</th>
+              <th class="px-4 py-3">Quantity</th>
+              <th class="px-4 py-3">Price</th>
+            </tr>
+          </thead>
+          <tbody class="bg-white"></tbody>
+        </table>
+      </div>
+    </div>
+  </section>
+</template>
